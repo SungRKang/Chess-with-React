0879ac11b0d9169d05d6c1d397f2c2ab76e9619c
SungRKang <sungwoo.kang@ucdenver.edu>
2023-11-07 19:26:44 -0700

    The board

--- chess-app/src/components/Chessboard.tsx
+++ chess-app/src/components/Chessboard.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import './Chessboard.css';
 
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
-const verticalAxis = [1,2,3,4,5,6,7,8];
+const verticalAxis = ["1","2","3","4","5","6","7","8"];
 
 
 export default function Chessboard() {
   let board = [];
-
+for(let j=verticalAxis.length-1; j>=0; j--) {
   for(let i=0; i<horizontalAxis.length; i++) {
-    for(let j=0; j<verticalAxis.length; j++) {
-      board.push(<span>{horizontalAxis[i]} {verticalAxis[j]}</span>)
+    const number = j+i+2;
+    if (number % 2 == 0) {
+      board.push(
+        <div className="tile black-tile"></div>
+        );
+    }
+    else {
+      board.push(
+        <div className="tile white-tile"></div>
+        );
+    }
+      
     }
   }
-  return <div id="chessboard">{board}</div>;
+  return <div id="chessboard"> {board}</div>;
 }
