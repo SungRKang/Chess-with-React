6707f4f296265d2882e55f7cf3ac04ee0dfeb4ec
SungRKang <sungwoo.kang@ucdenver.edu>
2023-11-08 09:21:12 -0700

    added piece movement, no collision

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Tile from '../Tile/Tile';
 import './Chessboard.css';
 
@@ -32,10 +33,40 @@ for (let i=0; i<8; i++) {
   pieces.push({image: "assets/images/pawn_w.png", x:i, y:1}); 
 }
 
+let activePiece: HTMLElement | null = null;
 
+function grabPiece(e: React.MouseEvent) {
+  const element = e.target as HTMLElement;
+  if(element.classList.contains("chess-piece")) {
+    const x = e.clientX - 50;
+    const y = e.clientY - 50;
+    element.style.position = "absolute";
+    element.style.left = `${x}px`;
+    element.style.top = `${y}px`
+
+    activePiece = element;
+  }
+}
+
+function movePiece(e: React.MouseEvent) {
+  if(activePiece) {
+    const x = e.clientX - 50;
+    const y = e.clientY - 50;
+    activePiece.style.position = "absolute";
+    activePiece.style.left = `${x}px`;
+    activePiece.style.top = `${y}px`
+  }
+} 
+
+function dropPiece(e: React.MouseEvent) {
+  if(activePiece) {
+    activePiece = null;
+  }
+}
   
 export default function Chessboard() {
   let board = [];
+
   for(let j=verticalAxis.length-1; j>=0; j--) {
     for(let i=0; i<horizontalAxis.length; i++) {
       const number = j+i+2;
@@ -47,8 +78,15 @@ export default function Chessboard() {
         }
       }) 
 
-      board.push(<Tile image={image} number={number}/>)
+      board.push(<Tile key={`${i},${j}`}image={image} number={number}/>)
     }
 }
-  return <div id="chessboard"> {board}</div>;
+  return (
+  <div 
+    onMouseMove={(e) => movePiece(e)} 
+    onMouseDown={(e) => grabPiece(e)}
+    onMouseUp={(e) => dropPiece(e)}  
+    id="chessboard"> {board}
+    </div>
+  );
 }
